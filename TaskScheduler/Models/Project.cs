6b5eb113de8349ff7f44a5bca2b6b97312b27e1a
Arun Vijayraghavan <arun.vijayraghavan@fh-heidelberg.de>
2015-10-11 21:42:14 +0200

    Editing project CRUD operations and creating the user model

--- TaskScheduler/Models/Project.cs
+++ TaskScheduler/Models/Project.cs
@@ -9,12 +9,17 @@ namespace TaskScheduler.Models
     public class Project
     {
         public int id { get; set; }
+        [Display(Name="Project name")]
         public string name { get; set; }
+        [Display(Name = "Assigned to")]
         public string assignedTo { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Start date")]
         public DateTime startDate { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "End date")]
         public DateTime endDate { get; set; }
+        [Display(Name = "Project status")]
         public int status { get; set; }
     }
 }
